1710976ad3656550251ffe7de5b57f184e8818c7
杨柳 <1545766219@qq.com>
2018-01-28 17:26:19 +0800

    轮播

--- js/index.js
+++ js/index.js
@@ -50,8 +50,13 @@ function mouseleave($button,$block,$btn){
 var $fs_list = $(".fs_list"),
     $fs_list_li = $("#fs .fs_list .fs_list_title li"),
     $fs_list_layout_wrap = $("#fs_list_layout_item"),
-    $fs_list_layout = $("#fs .fs_list #fs_list_layout_item .list_layout_item");
+    $fs_list_layout = $("#fs .fs_list #fs_list_layout_item .list_layout_item"),
+    $slide_li = $(".slide_list .slide_img li"),  //轮播图片li
+    $slide_left_button = $(".slide_wrap .slide_left_button"), //轮播上的左箭头
+    $slide_right_button = $(".slide_wrap .slide_right_button"), //轮播上的右箭头
+    $slide_indicators_btn = $(".slide_wrap .slide_indicators_btn");  //轮播西面的小圆点
 
+//菜单
 $fs_list_li.on("mouseenter",function(){
     var index = $(this).index();
     $(this).siblings().removeClass("hover");
@@ -76,3 +81,89 @@ $fs_list.on("mouseleave",function(){
     $fs_list_layout_wrap.css("display","none");
     $fs_list_layout.css("display","none");
 })
+//轮播
+slide();
+function slide(){
+    var slideindex = 1,
+        slideTime,
+        slideTime2,
+        slideTime3;
+
+    sliding();
+    $slide_indicators_btn.on("mouseenter",function(){
+        clearslideTime();
+        var index = $(this).index();
+            slideindex = index+1;
+        $(this).addClass("slide_indicators_active");
+        $(this).siblings().removeClass("slide_indicators_active");
+        $slide_li.eq(index).siblings().removeClass("slide_img_active");
+        $slide_li.eq(index).addClass("slide_img_active");
+        slideTime3 = setTimeout(function(){
+            slideTime2 = sliding();
+        },3000);
+    });
+    $slide_li.on("mouseenter",function(){
+        clearslideTime();
+    })
+    $slide_li.on("mouseleave",function(){
+        clearslideTime();
+        sliding();
+    })
+    $slide_left_button.on("click",function(){
+        clearslideTime();
+        slideindex = slideindex-2 ;
+        if(slideindex < 0){
+            slideindex = 7;
+        }else if(slideindex > 7){
+            slideindex = 0;
+        }
+
+        $slide_li.eq(slideindex).siblings().removeClass("slide_img_active");
+        setTimeout(function(){
+            $slide_li.eq(slideindex).addClass("slide_img_active");
+            $slide_indicators_btn.eq(slideindex).siblings().removeClass("slide_indicators_active");
+            $slide_indicators_btn.eq(slideindex).addClass("slide_indicators_active");
+            slideindex++;
+        },200)
+        sliding();
+    })
+    $slide_right_button.on("click",function(){
+        clearslideTime();
+        if(slideindex < 0){
+            slideindex = 7;
+        }else if(slideindex > 7){
+            slideindex = 0;
+        }
+
+        $slide_li.eq(slideindex).siblings().removeClass("slide_img_active");
+        setTimeout(function(){
+            $slide_li.eq(slideindex).addClass("slide_img_active");
+            $slide_indicators_btn.eq(slideindex).siblings().removeClass("slide_indicators_active");
+            $slide_indicators_btn.eq(slideindex).addClass("slide_indicators_active");
+            slideindex++;
+        },200)
+        sliding();
+    })
+    function sliding(){
+        slideTime = setInterval(function(){
+            if(slideindex > 7){
+                slideindex = 0;
+            }else if(slideindex < 0){
+                slideindex = 7;
+            }
+            $slide_li.eq(slideindex).siblings().removeClass("slide_img_active");
+            setTimeout(function(){
+                $slide_li.eq(slideindex).addClass("slide_img_active");
+                $slide_indicators_btn.eq(slideindex).siblings().removeClass("slide_indicators_active");
+                $slide_indicators_btn.eq(slideindex).addClass("slide_indicators_active");
+                slideindex++;
+            },200)
+        },3000);
+    }
+    //清除轮循时间
+    function clearslideTime(){
+        clearInterval(slideTime);
+        clearInterval(slideTime2);
+        clearTimeout(slideTime3);
+    }
+}
